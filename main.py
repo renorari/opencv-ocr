c3407ce0ffd287e2e087b201848a70666295ac21
renorari <renorari@renorari.net>
2024-11-23 12:08:10 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import sys
+import cv2
+import pyocr
+from PIL import Image
+
+# 画像ファイルのパス
+image = "test.png"
+
+# 画像ファイルの読み込み
+img = cv2.imread(image)
+
+# python ocrで使用するツールを取得(今回はtesseractを使用)
+tools = pyocr.get_available_tools()
+if len(tools) == 0:
+    print("No OCR tool found")
+    sys.exit(1)
+tool = tools[0]
+
+# 画像ファイルをOCRで読み込む
+res = tool.image_to_string(
+    Image.open("test.png")
+    ,lang="eng")
+
+# 結果を出力
+print(res)
